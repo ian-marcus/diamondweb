be7fc141dc6defe9bf7be057351e3631ceab9592
ian-marcus <ibeasianmarcus@gmail.com>
2021-10-01 20:23:38 +0800

    edit the after-timer message

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
               countdown = document.getElementById("countdown"),
               content = document.getElementById("content");
 
-          headline.innerText = "It's my birthday!";
+          headline.innerText = "Buy Diamond Swap Token Now!";
           countdown.style.display = "none";
           content.style.display = "block";
 
