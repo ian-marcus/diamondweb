f5a61ff0279c0491fa080e9b460afd4649001228
ian-marcus <ibeasianmarcus@gmail.com>
2021-10-07 12:35:33 +0800

    remove CA

--- script.js
+++ script.js
@@ -35,15 +35,15 @@
 
 // clipboard for CA
 
-function myFunction() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
+// function myFunction() {
+//   /* Get the text field */
+//   var copyText = document.getElementById("myInput");
 
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+//   /* Select the text field */
+//   copyText.select();
+//   copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
+//    /* Copy the text inside the text field */
+//   navigator.clipboard.writeText(copyText.value);
 
-}
+// }
